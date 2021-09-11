30f19be7abc891e1dfc6b171ccd5ec79f58aabb2
Erwin Ruiz <88996407+erwinruiz@users.noreply.github.com>
2021-09-10 21:45:57 -0700

    Make testimonials component responsive in desktop design

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -5,14 +5,16 @@ function Testimonials() {
   return (
     <section className={classes.container}>
       <h1>Client testimonials</h1>
-      {clients.map((client, i) => (
-        <div key={i} className={classes.testimonial}>
-          <img src={client.img} alt={client.altImg} />
-          <p className={classes.description}>{client.text}</p>
-          <h3>{client.name}</h3>
-          <p className={classes.role}>{client.role}</p>
-        </div>
-      ))}
+      <div className={classes.testimonials}>
+        {clients.map((client, i) => (
+          <div key={i} className={classes.testimonial}>
+            <img src={client.img} alt={client.altImg} />
+            <p className={classes.description}>{client.text}</p>
+            <h3>{client.name}</h3>
+            <p className={classes.role}>{client.role}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
